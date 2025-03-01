35249e6c7c53aa7ac55fe103f517ce394df30887
정필성 <ibetter.kr@gmail.com>
2025-03-01 15:12:20 +0900

    init

---
+++ src/main/java/com/example/spring/posts/PostsVo.java
@@ -0,0 +1,67 @@
+package com.example.spring.posts;
+
+public class PostsVo {
+    private int id;
+    private String title;
+    private String content;
+    private String username;
+    private String password;
+    private String createdAt;
+    private String updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
